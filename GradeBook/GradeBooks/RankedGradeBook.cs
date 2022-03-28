aa374d4bfa7c7ec4e58981aa74129735000e39f8
Mateusz Jaroszko <matt7269@protonmail.com>
2022-03-28 19:16:20 +0200

    Creating the RankedGradeBook class

---
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Text;
+namespace GradeBook.GradeBooks
+{
+	public class RankedGradeBook : BaseGradeBook
+	{
+		public RankedGradeBook(string name) : base(name)
+		{
+			Type = GradeBookType.Ranked;
+		}
+	}
+}
