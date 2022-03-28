4e4a74e050a21bfd52c2d73470aa85bf9b31edaa
Mateusz Jaroszko <matt7269@protonmail.com>
2022-03-28 19:12:32 +0200

    Creating the StandardGradeBook class

---
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Text;
+namespace GradeBook.GradeBooks
+{
+	public class StandardGradeBook : BaseGradeBook
+	{
+		public StandardGradeBook(string name) : base(name)
+        {
+			Type = GradeBookType.Standard;
+        }
+	}
+}
